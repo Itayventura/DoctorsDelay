be50cb18eabb3eaa100b7adb9c92e63dd6c8ce93
michaldeutch <michaldeutch@gmail.com>
2019-11-22 15:06:49 +0200

    API throws exceptions and returns int's

--- Algorithms/src/main/java/Algorithms.java
+++ Algorithms/src/main/java/Algorithms.java
@@ -2,36 +2,44 @@ import java.time.LocalDateTime;
 
 public interface Algorithms {
 
-    public abstract class Response{
-        public enum ResponseStatus{
-            SUCCESSFUL,
+    public class AlgorithmException extends Exception{
+        public enum Reason{ //TODO - if you want, you can add\remove reasons and just push
             DOCTOR_NOT_EXISTS,
             NO_CURRENT_DATA,
             NO_DATA_FOUND
         }
-        private int expected_delay;
-        private int current_delay;
-        public abstract int getExpected_delay();
-        public abstract int getCurrent_delay();
+
+        public AlgorithmException(Reason reason, String error) {
+            super(error);
+            this.reason = reason;
+        }
+
+        public AlgorithmException(Reason reason) {
+            super();
+            this.reason = reason;
+        }
+
+        private Reason reason;
+        public Reason getReason() { return reason; }
     }
 
     /**
      * @param doctorsName - may not exist
-     * @return the current estimated time according to reports
+     * @return the current estimated time according to reports - in minutes
      */
-    Response getCurrentDelay(String doctorsName);
+    int getCurrentDelay(String doctorsName) throws AlgorithmException;
 
     /**
      * @param doctorsName - may not exist
      * @param meetingDateTime - to determine what the expected delay at that time will be.
-     * @return the estimated time according to history
+     * @return the estimated time according to history - in minutes
      */
-    Response getEstimatedDelay(String doctorsName, LocalDateTime meetingDateTime);
+    int getEstimatedDelay(String doctorsName, LocalDateTime meetingDateTime) throws AlgorithmException;
 
     /**
      * Straight forward - using DB addReport to set the records.
      * @param doctorsName
-     * @param reportedDelay
+     * @param reportedDelay - in minutes
      */
-    void addReport(String doctorsName, int reportedDelay);
+    void addReport(String doctorsName, int reportedDelay) throws AlgorithmException;
 }
