cd01a08e32c4f8e15c43ea047a6eb36776cd65ba
michaldeutch <michaldeutch@gmail.com>
2020-01-10 13:35:35 +0200

    Revert "changing tables in DB and connection pool"
    This reverts commit fe36fd80

--- Algorithms/src/main/java/algorithms/AlgorithmsImpl.java
+++ Algorithms/src/main/java/algorithms/AlgorithmsImpl.java
@@ -178,13 +178,14 @@ public class AlgorithmsImpl implements Algorithms {
             throw new AlgorithmException(AlgorithmException.Reason.DOCTOR_NOT_EXISTS);
         }
 
-        Duration duration = Duration.between(LocalDateTime.now().minusHours(1), meetingDateTime);
+        Duration duration = Duration.between(LocalDateTime.now(), meetingDateTime);
 
         if(isMeetingTimePassed(duration) || !isMeetingTimeInDoctorWorkRange(doctorName, meetingDateTime))
         {
             logger.info("Invalid data time: Prediction request time has passed.");
             throw new AlgorithmException(AlgorithmException.Reason.INVALID_TIME_REQUEST);
         }
+
     }
 
     protected Boolean isMeetingTimePassed(Duration duration)
